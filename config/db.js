b3fdba2461081ea1e57c2557e40c37066e234ea1
leandra25 <40801089+leandra25@users.noreply.github.com>
2024-11-12 18:53:36 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,10 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,  // Esto toma el valor de la variable de entorno DB_HOST
-  user: process.env.DB_USER,  // Toma el valor de DB_USER
-  password: process.env.DB_PASSWORD,  // Toma el valor de DB_PASSWORD
-  database: process.env.DB_NAME,  // Toma el valor de DB_NAME
-  port: process.env.DB_PORT || 3306 // Puerto de la base de datos, por defecto es 3306 para MySQL
-});
+const mysql = require('mysql2/promise');
 
-connection.connect(err => {
-  if (err) {
-    console.error('Error de conexión a la base de datos:', err.stack);
-  } else {
-    console.log('Conexión exitosa a la base de datos');
-  }
+const db = mysql.createPool({
+    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER ||'consulta',
+    password: process.env.MYSQL_PASSWORD ||'1234',
+    database: process.env.MYSQL_DB || 'consula_agenda'
 });
 
 module.exports = db;
