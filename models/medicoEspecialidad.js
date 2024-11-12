21812a437e0ff943bcbd8c8ed7eff5c868662d08
leandra25 <40801089+leandra25@users.noreply.github.com>
2024-11-12 20:59:43 -0300

    Update medicoEspecialidad.js

--- models/medicoEspecialidad.js
+++ models/medicoEspecialidad.js
@@ -1,30 +1,44 @@
+const db = require('../config/db');  
+
 class MedicoEspecialidad {
-    constructor(id_medico_especialidad, id_medico, id_especialidad, matricula, fecha_matricula) {
-      this.id_medico_especialidad = id_medico_especialidad;
-      this.id_medico = id_medico;
-      this.id_especialidad = id_especialidad;
-      this.matricula = matricula;
-      this.fecha_matricula = fecha_matricula;
-    }
+  constructor(id_medico_especialidad, id_medico, id_especialidad, matricula, fecha_matricula) {
+    this.id_medico_especialidad = id_medico_especialidad;
+    this.id_medico = id_medico;
+    this.id_especialidad = id_especialidad;
+    this.matricula = matricula;
+    this.fecha_matricula = fecha_matricula;
+  }
+
   
-    static async getAll() {
-      const [rows] = await connection.promise().query(`
+  static async getAll() {
+    const connection = await db;  
+    try {
+      const [rows] = await connection.query(`
         SELECT me.*, m.nombre AS medico_nombre, e.descripcion AS especialidad_descripcion
         FROM medico_especialidad me
         JOIN medicos m ON me.id_medico = m.id
         JOIN especialidades e ON me.id_especialidad = e.id_especialidad
       `);
-      return rows;
+      return rows; 
+    } finally {
+      await connection.end(); 
     }
+  }
+
   
-    static async create(id_medico, id_especialidad, matricula, fecha_matricula) {
-      const [result] = await connection.promise().query(`
+  static async create(id_medico, id_especialidad, matricula, fecha_matricula) {
+    const connection = await db;  
+    try {
+      const [result] = await connection.query(`
         INSERT INTO medico_especialidad (id_medico, id_especialidad, matricula, fecha_matricula)
         VALUES (?, ?, ?, ?)`, [id_medico, id_especialidad, matricula, fecha_matricula]
       );
-      return result;
+      return result; 
+    } finally {
+      await connection.end(); 
     }
   }
-  
-  module.exports = MedicoEspecialidad;
-  
+}
+
+module.exports = MedicoEspecialidad;
+
