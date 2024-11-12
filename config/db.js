6ffafb0271ee82f017fd05454356fa893f5731fc
leandra25 <40801089+leandra25@users.noreply.github.com>
2024-11-12 20:56:14 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
 const mysql = require('mysql2/promise');
 
-const db = mysql.createPool({
-    host: process.env.MYSQL_HOST ,
-    user: process.env.MYSQL_USER ,
-    password: process.env.MYSQL_PASSWORD ,
-    database: process.env.MYSQL_DB ,
-    port: process.env.MYSQL_PORT,
-  waitForConnections: true,  // Esperar si no hay conexiones disponibles
-  connectionLimit: 1000,       // Límite de conexiones en el pool
-  queueLimit: 0
+const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    port: process.env.MYSQL_PORT
 });
 
 module.exports = db;
