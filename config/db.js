1fe38a5fe29a540919abbcf406ce7a14d620b33d
leandra25 <40801089+leandra25@users.noreply.github.com>
2024-11-13 13:55:44 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,10 @@ const db = mysql.createPool({
     user: process.env.MYSQL_USER ,
     password: process.env.MYSQL_PASSWORD ,
     database: process.env.MYSQL_DB,
-    port: process.env.MYSQL_PORT 
+    port: process.env.MYSQL_PORT,
+    waitForConnections: true,
+  connectionLimit: 10,  
+  queueLimit: 0,
 });
 
 module.exports = db;
