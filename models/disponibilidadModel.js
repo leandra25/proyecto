23c53bce7993d3dcf032e195ff092d1d69981f02
leandra25 <40801089+leandra25@users.noreply.github.com>
2024-11-13 13:52:34 -0300

    Update disponibilidadModel.js

--- models/disponibilidadModel.js
+++ models/disponibilidadModel.js
@@ -1,21 +1,17 @@
-const db = require('../config/db');  
+const db = require('../config/db');
 
 class Disponibilidad {
-  static async obtenerHorariosPorMedico(id_medico, fecha) {
-    const connection = await db;  
-    const query = `
-      SELECT fecha, hora 
-      FROM disponibilidad
-      WHERE id_medicos = ? 
-      AND fecha >= CURDATE()  
-      AND (fecha = ? OR ? IS NULL) `;
-try {      
-      const [horarios] = await connection.query(query, [id_medico, fecha, fecha]);
-      return horarios; 
-    } finally {
-      await connection.end();  
+    static async obtenerHorariosPorMedico(id_medico, fecha) {
+        const query = `
+            SELECT fecha, hora 
+            FROM disponibilidad
+            WHERE id_medicos = ? 
+            AND fecha >= CURDATE()  
+            AND (fecha = ? OR ? IS NULL)  
+        `;
+        const [horarios] = await db.query(query, [id_medico, fecha, hora]);
+        return horarios;
     }
-  }
 }
 
 module.exports = Disponibilidad;
