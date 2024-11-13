ee7dc376ddf6d19ab170b730a1ae87428b04193f
leandra25 <40801089+leandra25@users.noreply.github.com>
2024-11-12 21:04:26 -0300

    Update especialidadModel.js

--- models/especialidadModel.js
+++ models/especialidadModel.js
@@ -1,30 +1,53 @@
-const db = require('../config/db');
+const db = require('../config/db');  
 
 class Especialidad {
-    static async all() {
-        const [results] = await db.query('SELECT * FROM especialidad');
-        return results;
+  static async all() {
+    const connection = await db;  
+    try {
+      const [results] = await connection.query('SELECT * FROM especialidad');
+      return results;  
+    } finally {
+      await connection.end();  
     }
+  }
+  static async create(descripcion) {
+    const connection = await db;  
+    const query = 'INSERT INTO especialidad (descripcion) VALUES (?)';
+    try {
+      await connection.query(query, [descripcion]);
+    } finally {
+      await connection.end(); 
+    }
+  }
+  static async findById(id) {
+    const connection = await db;  
+    try {
+      const [results] = await connection.query('SELECT * FROM especialidad WHERE id_especialidad = ?', [id]);
+      return results[0];  
+    } finally {
+      await connection.end(); 
+    }
+  }
 
-    static async create(descripcion) {
-        const query = 'INSERT INTO especialidad (descripcion) VALUES (?)';
-        await db.query(query, [descripcion]);
+  static async update(id, descripcion) {
+    const connection = await db;  
+    const query = 'UPDATE especialidad SET descripcion = ? WHERE id_especialidad = ?';
+    try {
+      await connection.query(query, [descripcion, id]);
+    } finally {
+      await connection.end();  
     }
+  }
 
-    static async findById(id) {
-        const [results] = await db.query('SELECT * FROM especialidad WHERE id_especialidad = ?', [id]);
-        return results[0];
+  static async delete(id) {
+    const connection = await db; 
+    const query = 'DELETE FROM especialidad WHERE id_especialidad = ?';
+    try {
+      await connection.query(query, [id]);
+    } finally {
+      await connection.end();  
     }
-
-    static async update(id, descripcion) {
-        const query = 'UPDATE especialidad SET descripcion = ? WHERE id_especialidad = ?';
-        await db.query(query, [descripcion, id]);
-    }
-
-    static async delete(id) {
-        const query = 'DELETE FROM especialidad WHERE id_especialidad = ?';
-        await db.query(query, [id]);
-    }
+  }
 }
 
 module.exports = Especialidad;
