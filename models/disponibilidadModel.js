832c2d2a03787858a8f491866388b5e5405af6eb
leandra25 <40801089+leandra25@users.noreply.github.com>
2024-11-13 10:20:22 -0300

    Update disponibilidadModel.js

--- models/disponibilidadModel.js
+++ models/disponibilidadModel.js
@@ -9,9 +9,7 @@ class Disponibilidad {
       WHERE id_medicos = ? 
       AND fecha >= CURDATE()  
       AND (fecha = ? OR ? IS NULL) `;
-
-    try {
-      
+try {      
       const [horarios] = await connection.query(query, [id_medico, fecha, fecha]);
       return horarios; 
     } finally {
